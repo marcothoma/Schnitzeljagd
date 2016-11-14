9c40f4a5db398608c57e976aa4d5f1d943781de7
marcothoma <marcothoma98@gmail.com>
2016-11-14 15:01:29 +0100

    gameuebersicht mit daten aus db abgefüllt

--- Frontend/js/directivesAndControllers/goOnSchnitzeljagdDirectiveAndController.js
+++ Frontend/js/directivesAndControllers/goOnSchnitzeljagdDirectiveAndController.js
@@ -8,6 +8,8 @@ myApp.directive('sjGoOnSchnitzeljagd', function() {
         templateUrl: '../Frontend/templates/goOnSchnitzeljagdView.html'
     };
 });
-myApp.controller('goOnSchnitzeljagdController', function($scope) {
-    $scope.games = [{name: "Test1", points: "5", upvotes: "40"}, {name: "Test2", points: "8", upvotes: "20"}, {name: "Test3", points: "9", upvotes: "77"}]
+myApp.controller('goOnSchnitzeljagdController', function($scope, $http) {
+    $http.get("../Backend/index.php?action=gameList").success(function(games){
+        $scope.games = games;
+    });
 });
