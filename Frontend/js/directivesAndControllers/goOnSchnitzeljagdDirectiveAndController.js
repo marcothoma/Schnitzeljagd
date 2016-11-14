64c09003c086b4b6361b6e027a2334060fc82154
Yingrjimsch <nobel98@bluewin.ch>
2016-11-14 16:16:50 +0100

    richtig vill sache gmacht

--- Frontend/js/directivesAndControllers/goOnSchnitzeljagdDirectiveAndController.js
+++ Frontend/js/directivesAndControllers/goOnSchnitzeljagdDirectiveAndController.js
@@ -9,5 +9,14 @@ myApp.directive('sjGoOnSchnitzeljagd', function() {
     };
 });
 myApp.controller('goOnSchnitzeljagdController', function($scope) {
-    $scope.games = [{name: "Test1", points: "5", upvotes: "40"}, {name: "Test2", points: "8", upvotes: "20"}, {name: "Test3", points: "9", upvotes: "77"}]
+
+    $http.get("../Backend/index.php?action=gameList").success(function(games){
+        $scope.games = games;
+    });
+
+    $scope.setClicked = function(rbID) {
+        console.log("radio_" + rbID);
+        var radioButtonToSelect = document.getElementById("radio_" + rbID);
+        radioButtonToSelect.checked = true;
+    }
 });
