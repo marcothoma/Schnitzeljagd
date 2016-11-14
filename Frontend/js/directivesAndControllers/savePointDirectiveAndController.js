5f442e475ec234d969c11b2e362ea8064fda7389
Yingrjimsch <nobel98@bluewin.ch>
2016-11-14 09:33:44 +0100

    savePoint gerüst gemacht

---
+++ Frontend/js/directivesAndControllers/savePointDirectiveAndController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Anwender on 14.11.2016.
+ */
+myApp.directive('sjSavePoint', function() {
+
+    return {
+        controller: 'savePointController',
+        templateUrl: '../Frontend/templates/savePointView.html'
+    };
+});
+myApp.controller('savePointController', function() {
+
+});
