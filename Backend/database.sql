88f53b97d69a416b5e0555c905630e4cff8b4f52
marcothoma <marcothoma98@gmail.com>
2016-11-18 14:08:28 +0100

    utf-8

--- Backend/database.sql
+++ Backend/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 18. Nov 2016 um 11:42
+-- Erstellungszeit: 18. Nov 2016 um 14:07
 -- Server-Version: 5.6.24
 -- PHP-Version: 5.6.8
 
@@ -11,17 +11,15 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
 --
 -- Datenbank: `schnitzeljagd`
 --
-CREATE DATABASE IF NOT EXISTS `schnitzeljagd`
-  DEFAULT CHARACTER SET latin1
-  COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `schnitzeljagd` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `schnitzeljagd`;
 
 -- --------------------------------------------------------
@@ -32,14 +30,11 @@ USE `schnitzeljagd`;
 
 DROP TABLE IF EXISTS `games`;
 CREATE TABLE IF NOT EXISTS `games` (
-  `id`           INT(11)    NOT NULL,
-  `name`         TEXT       NOT NULL,
-  `isPublic`     TINYINT(1) NOT NULL,
-  `ratingPoints` INT(11) DEFAULT NULL
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 3
-  DEFAULT CHARSET = latin1;
+  `id` int(11) NOT NULL,
+  `name` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `isPublic` tinyint(1) NOT NULL,
+  `ratingPoints` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `games`
@@ -57,15 +52,12 @@ INSERT INTO `games` (`id`, `name`, `isPublic`, `ratingPoints`) VALUES
 
 DROP TABLE IF EXISTS `points`;
 CREATE TABLE IF NOT EXISTS `points` (
-  `id`           INT(11) NOT NULL,
-  `fk_games`     INT(11) NOT NULL,
-  `description`  TEXT    NOT NULL,
-  `xCoordinates` TEXT    NOT NULL,
-  `yCoordinates` TEXT    NOT NULL
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 10
-  DEFAULT CHARSET = latin1;
+  `id` int(11) NOT NULL,
+  `fk_games` int(11) NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `xCoordinates` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `yCoordinates` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `points`
@@ -74,12 +66,8 @@ CREATE TABLE IF NOT EXISTS `points` (
 INSERT INTO `points` (`id`, `fk_games`, `description`, `xCoordinates`, `yCoordinates`) VALUES
   (3, 1, 'Gehe nach Links in die Vandalia', '-73.866516', '40.658634'),
   (4, 1, 'Gehe nach Links bis zur Bushaltestelle: FOUNTAIN AV/FLATLANDS AV', '-73.866891', '40.660278'),
-  (5, 1,
-   'Überquere die Kreuzung und gehe bis richts die Old Mill Rd erscheint. Laufe an dieser Strasse vorbei bis zur nächsten Kreuzung',
-   ' -73.868323', '40.661995'),
-  (6, 1,
-   'Gehe an der Kreuzung nach rechts und dann nach Links in die Pine Street. Bei der Amazing Grace Church haben Sie Ihr Ziel erreicht.',
-   '-73.867832', '40.662933'),
+  (5, 1, 'Überquere die Kreuzung und gehe bis richts die Old Mill Rd erscheint. Laufe an dieser Strasse vorbei bis zur nächsten Kreuzung', ' -73.868323', '40.661995'),
+  (6, 1, 'Gehe an der Kreuzung nach rechts und dann nach Links in die Pine Street. Bei der Amazing Grace Church haben Sie Ihr Ziel erreicht.', '-73.867832', '40.662933'),
   (7, 2, 'Laufe an der Limat entlang bis zur Münsterbrücke', '8.541872', '47.369868'),
   (8, 2, 'Überquere die Münsterbrücke und laufe weiter bis zur ersten Querstrasse', '8.543256', '47.370046'),
   (9, 2, 'Gehe Nach Links bis zum Restaurant zum Rüden. Guten Appetit;)', '8.542999', '47.370903');
@@ -108,14 +96,12 @@ ALTER TABLE `points`
 -- AUTO_INCREMENT für Tabelle `games`
 --
 ALTER TABLE `games`
-  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT für Tabelle `points`
 --
 ALTER TABLE `points`
-  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 10;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
