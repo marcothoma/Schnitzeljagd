b65d2a7dd28a7ece83019dc4ea80877f4b9012ec
marcothoma <marcothoma98@gmail.com>
2016-11-18 15:09:12 +0100

    bugfix

--- Backend/controller/createSchnitzeljagd.php
+++ Backend/controller/createSchnitzeljagd.php
@@ -11,7 +11,12 @@ class Controller {
     public function saveSchnitzeljagd($props) {
         $model = new Model();
         $gameID = $model->getNextGameID();
-        $model->saveGame($props->gameName, $props->isPublic, '0');
+        if ($props->isPublic) {
+            $isPublic = 1;
+        } else {
+            $isPublic = 0;
+        }
+        $model->saveGame($props->gameName, $isPublic, '0');
         foreach ($props->points as $point) {
             $model->savePoint($gameID, $point['0'], $point['1'], $point['2']);
         }
